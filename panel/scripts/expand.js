44f92aecdbe11ccd46d224dbc43de403fb8ac31d
Cristian <cristianarevaloduran@gmail.com>
2021-12-11 01:32:05 -0500

    Muxa cosa

--- panel/scripts/expand.js
+++ panel/scripts/expand.js
@@ -1,11 +1,15 @@
 const profileModalButton1 = document.getElementById("profileModalButton1");
 const profileModalButton2 = document.getElementById("profileModalButton2");
+const profileModalButton3 = document.getElementById("profileModalButton3");
+const profileModalButton4 = document.getElementById("profileModalButton4");
 const profileModal = document.getElementById("profileModal");
 const profileModalClose = document.getElementById("profileModalClose");
 const profileModalContent = document.getElementById("profileModalContent");
 
 profileModalButton1.addEventListener("click", openModal);
 profileModalButton2.addEventListener("click", openModal);
+profileModalButton3.addEventListener("click", openModal);
+profileModalButton4.addEventListener("click", openModal);
 profileModalClose.addEventListener("click", closeModal);
 
 function openModal(event) {
