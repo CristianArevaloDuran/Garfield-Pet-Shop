9c87f9a7c016e0477384a710b296d3a68c5ecbf8
juan <juan@gmail.com>
2022-05-06 11:25:39 -0500

    Roles

--- panel/modules/ModuloRoles/add/addQuery.php
+++ panel/modules/ModuloRoles/add/addQuery.php
@@ -4,21 +4,43 @@
     include("../../../../db/dbConnection.php");
 
     $name = $_POST["name"];
-    $checks = $_POST["checks"];
+    if(isset($_POST["checks"])){
+        $checks = $_POST["checks"];
+    }
+    $output = array();
 
-    if($checks) {
-        
-            echo json_encode(array("bool" => false, "value" => "$checks[0]"));
-        
+    if($name == "") {
+        echo json_encode(array("bool" => false, "value" => "El campo nombre es obligatorio"));
     } else {
-        $query = $conexion->prepare("");
+        $query = $conexion->prepare("SELECT nombreRol FROM tblRol WHERE nombreRol = '$name'");
         $query->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $query->execute();
-        if($result) {
-            echo json_encode(array("bool" => true, "value" => "Datos actualizados"));
+        $query->execute();
+        $result = $query->fetch();
+        if(!empty($result)) {
+            echo json_encode(array("bool" => false, "value" => "Este rol ya existe"));
         } else {
-            echo json_encode(array("bool" => false, "value" => "Error al actualizar, vuelva a intentar"));
+            if (!isset($checks)) {
+                echo json_encode(array("bool" => false, "value" => "Escoja al menos un privilegio"));
+            } else {
+                foreach($checks as $check) {
+                    array_push($output, $check);
+                }
+                $query2 = $conexion->prepare("INSERT INTO tblrol(`nombreRol`) VALUES ('$name')");
+                $query2->execute();
+                if($query2) {
+                    foreach($output as $out) {
+                        $query3 = $conexion->prepare("INSERT INTO `tblrolprivilegio`(`idRol`, `idPrivilegio`) VALUES ('$name','$out') WHERE ");
+                        $query3->execute();
+                    }
+                    if($query2) {
+                        echo json_encode(array("bool" => tru, "value" => "Rol registrado"));
+                    } else {
+    
+                    }
+                } else {
+                    echo json_encode(array("bool" => false, "value" => "Error"));
+                }
+            }
         }
-        
     }
 ?>
